6c81bdc77c1a370e75d55e0c5f87f8bd909848de
Mário César Silva <mcgato33@hotmail.com>
2023-01-08 07:56:29 -0100

    NestJS integrado ao Prisma como ORM

--- notifications-service/src/app.controller.ts
+++ notifications-service/src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
-import { MailService } from './mail/mail.service';
+import { Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
-@Controller('app')
+@Controller('notification')
 export class AppController {
-  constructor(private readonly mailService: MailService) {}
+  constructor(private readonly prisma: PrismaService) {}
 
-  @Get('hello')
-  getHello(): string {
-    return this.mailService.sendEmail();
+  @Get()
+  list() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create() {
+    await this.prisma.notification.create({
+      data: {
+        content: 'Você tem uma nova solicitação de amizade',
+        category: 'social',
+        recipientId: '3313',
+      }
+    });
   }
 }
